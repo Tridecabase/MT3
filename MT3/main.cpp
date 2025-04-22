91ef6f663de007af156e55df00ee135c7ca2b44d
Stargazerxiii-design <78391698+Tridecabase@users.noreply.github.com>
2025-04-22 12:39:49 +0900

    update

--- MT3/main.cpp
+++ MT3/main.cpp
@@ -1,79 +1,90 @@
 #include <Novice.h>
 #include "Vector3.h"
-#include <math.h>
+#include <cmath>
+#include "Matrix4x4.h"
 
-const char kWindowTitle[] = "GC1B_08_ジョ_シセイ";
+const char kWindowTitle[] = "GC2B_05_ジョ_シセイ";
 
 /// <summary>
-/// 三次元ベクトルの加算
+/// x軸回りの回転行列を作成
 /// </summary>
-/// <param name="v1"></param>
-/// <param name="v2"></param>
-/// <returns>加算結果</returns>
-Vector3 Add(const Vector3& v1, const Vector3& v2) {
-	return Vector3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+/// <param name="radian">角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 matrix = {};
+	matrix.m[0][0] = 1.0f;
+	matrix.m[1][1] = std::cos(radian);
+	matrix.m[1][2] = std::sinf(radian);
+	matrix.m[2][1] = -std::sinf(radian);
+	matrix.m[2][2] = std::cosf(radian);
+	matrix.m[3][3] = 1.0f;
+	return matrix;
 }
 
 /// <summary>
-/// 三次元ベクトルの減算
+/// y軸回りの回転行列を作成
 /// </summary>
-/// <param name="v1"></param>
-/// <param name="v2"></param>
-/// <returns>減算結果</returns>
-Vector3 Subtract(const Vector3& v1, const Vector3& v2) {
-	return Vector3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+/// <param name="radian">角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 matrix = {};
+	matrix.m[0][0] = std::cos(radian);
+	matrix.m[0][2] = -std::sinf(radian);
+	matrix.m[1][1] = 1.0f;
+	matrix.m[2][0] = std::sinf(radian);
+	matrix.m[2][2] = std::cosf(radian);
+	matrix.m[3][3] = 1.0f;
+	return matrix;
 }
 
 /// <summary>
-/// 三次元ベクトルのスカラー倍
+/// z軸回りの回転行列を作成
 /// </summary>
-/// <param name="scalar"></param>
-/// <param name="v"></param>
-/// <returns>スカラー倍後のベクトル</returns>
-Vector3 Multiply(float scalar, const Vector3& v) {
-	return Vector3(scalar * v.x, scalar * v.y, scalar * v.z);
+/// <param name="radian">角度</param>
+/// <returns>回転行列</returns>
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 matrix = {};
+	matrix.m[0][0] = std::cos(radian);
+	matrix.m[0][1] = std::sinf(radian);
+	matrix.m[1][0] = -std::sinf(radian);
+	matrix.m[1][1] = std::cosf(radian);
+	matrix.m[2][2] = 1.0f;
+	matrix.m[3][3] = 1.0f;
+	return matrix;
 }
 
 /// <summary>
-/// 三次元ベクトルの内積
+/// 行列の掛け算
 /// </summary>
-/// <param name="v1"></param>
-/// <param name="v2"></param>
-/// <returns>内積計算の結果</returns>
-float Dot(const Vector3& v1, const Vector3& v2) {
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
-
-/// <summary>
-/// 三次元ベクトルの長さを計算する
-/// </summary>
-/// <param name="v"></param>
-/// <returns></returns>
-float Length(const Vector3& v) {
-	return sqrtf(Dot(v, v));
-}
-
-/// <summary>
-/// 三次元ベクトルの正規化
-/// </summary>
-/// <param name="v"></param>
-/// <returns>正規化後のベクトル</returns>
-Vector3 Normalize(const Vector3& v) {
-	float length = Length(v);
-	if (length == 0.0f) {
-		return Vector3(0.0f, 0.0f, 0.0f);
+/// <param name="x">行列x</param>
+/// <param name="y">行列y</param>
+/// <returns>掛け算結果</returns>
+Matrix4x4 Muiltiply(const Matrix4x4& x, const Matrix4x4& y) {
+	Matrix4x4 result = {};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = x.m[i][0] * y.m[0][j] + x.m[i][1] * y.m[1][j] +
+				x.m[i][2] * y.m[2][j] + x.m[i][3] * y.m[3][j];
+		}
 	}
-	return Multiply(1.0f / length, v);
+	return result;
 }
 
-static const int kColumWidth = 60;
 static const int kRowHeight = 20;
-// ベクトルを画面に表示する関数
-void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
-	Novice::ScreenPrintf(x, y, "%0.2f", vector.x);
-	Novice::ScreenPrintf(x + kColumWidth, y, "%0.2f", vector.y);
-	Novice::ScreenPrintf(x + kColumWidth * 2, y, "%0.2f", vector.z);
-	Novice::ScreenPrintf(x + kColumWidth * 3, y, "%s", label);
+static const int kColumnWidth = 60;
+/// <summary>
+/// 行列をスクリーンに表示
+/// </summary>
+/// <param name="x">行数</param>
+/// <param name="y">列数</param>
+/// <param name="m">4x4行列</param>
+void MatrixScreenPrintf(int x, int y, Matrix4x4& m, const char* label) {
+	Novice::ScreenPrintf(x, y, "%s", label);
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + (row + 1) * kRowHeight, "%6.02f", m.m[row][column]);
+		}
+	}
 }
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -99,16 +110,11 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		Vector3 v1{ 1.0f,3.0f,-5.0f };
-		Vector3 v2{ 4.0f,-1.0f,2.0f };
-		float k = { 4.0f };
-
-		Vector3 resultAdd = Add(v1, v2);
-		Vector3 resultSubtract = Subtract(v1, v2);
-		Vector3 resultMultiply = Multiply(k, v1);
-		float resultDot = Dot(v1, v2);
-		float resultLength = Length(v1);
-		Vector3 resultNormalize = Normalize(v2);
+		Vector3 rotate{ 0.4f,1.43f,-0.8f };
+		Matrix4x4 rotateXmatrix = MakeRotateXMatrix(rotate.x);
+		Matrix4x4 rotateYmatrix = MakeRotateYMatrix(rotate.y);
+		Matrix4x4 rotateZmatrix = MakeRotateZMatrix(rotate.z);
+		Matrix4x4 rotateXYZMatrix = Muiltiply(rotateXmatrix, Muiltiply(rotateYmatrix, rotateZmatrix));
 
 		///
 		/// ↑更新処理ここまで
@@ -118,12 +124,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		VectorScreenPrintf(0, 0, resultAdd, " : Add");
-		VectorScreenPrintf(0, kRowHeight, resultSubtract, " : Subtract");
-		VectorScreenPrintf(0, kRowHeight * 2, resultMultiply, "	: Multiply");
-		Novice::ScreenPrintf(0, kRowHeight * 3, "%0.2f : Dot", resultDot);
-		Novice::ScreenPrintf(0, kRowHeight * 4, "%0.2f : Length", resultLength);
-		VectorScreenPrintf(0, kRowHeight * 5, resultNormalize, " : Normalize");
+		MatrixScreenPrintf(0, 0, rotateXmatrix, "rotateXmatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, rotateYmatrix, "rotateYmatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, rotateZmatrix, "rotateZmatrix");
+		MatrixScreenPrintf(0, kRowHeight * 15, rotateXYZMatrix, "rotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
